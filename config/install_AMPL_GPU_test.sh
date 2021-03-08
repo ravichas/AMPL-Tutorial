749050bc22e3cd201b4493e7ac0bb9181fdde9e7
Ravichandran Sarangan <saka.ravi@gmail.com>
2021-03-08 16:25:08 -0500

    Configuration files

--- config/install_AMPL_GPU_test.sh
+++ config/install_AMPL_GPU_test.sh
@@ -1,4 +1,4 @@
-wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.3_Linux-x86_64.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.3-Linux-x86_64.sh
 bash Miniconda3-py37_4.8.3_Linux-x86_64.sh -b -p /content/AMPL
 
 cat << "EOF" > AMPL.txt
