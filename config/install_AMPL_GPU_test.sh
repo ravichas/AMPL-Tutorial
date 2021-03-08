64a154974e748c4ec17ede0c71b402cd055eb377
Ravichandran Sarangan <saka.ravi@gmail.com>
2021-03-08 16:59:29 -0500

    configuration files

--- config/install_AMPL_GPU_test.sh
+++ config/install_AMPL_GPU_test.sh
@@ -1,5 +1,5 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.3-Linux-x86_64.sh
-bash Miniconda3-py37_4.8.3-Linux-x86_64.sh -b -p -f /content/AMPL
+bash Miniconda3-py37_4.8.3-Linux-x86_64.sh -p -f /content/AMPL -b
 
 cat << "EOF" > AMPL.txt
 # This file may be used to create an environment using:
